27413e52b10c553961e905cdb3a321c3ef797863
Nikita Zhidov <nzhidov02@gmail.com>
2026-02-22 12:30:33 +0200

    Update favicon and add app logo

--- apps/web/src/shared/components/AppSidebar.tsx
+++ apps/web/src/shared/components/AppSidebar.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { ArrowLeftFromLine, ArrowRightFromLine } from 'lucide-react';
-import { useState } from 'react';
+import Image from 'next/image';
+import { useMemo, useState } from 'react';
 
 import { Button } from '@my-lex/ui';
 
+import appLogo from '../../../public/app-logo/app-logo-sm.png';
 import { cn, setCookie } from '../utils';
 
 import { APP_NAME, STORAGE_KEYS } from '@/constants';
@@ -14,38 +16,102 @@ export interface AppSidebarProps {
   initialCollapsedState: boolean;
 }
 
+const APP_LOGO_SIZE = 36;
+
+const collapsedSidebarWidthClass = 'w-14';
+const collapsedSidebarMaxWidthClass = 'max-w-14';
+
 export const AppSidebar = ({
   className,
   initialCollapsedState,
 }: AppSidebarProps) => {
   const [collapsed, setCollapsed] = useState(initialCollapsedState);
 
-  const toggleCollapsed = () => {
-    const newState = !collapsed;
+  const toggleCollapsed = () => updateCollapsedState(!collapsed);
 
+  const updateCollapsedState = (newState: boolean) => {
     setCookie(STORAGE_KEYS.APP_SIDEBAR_COLLAPSED, JSON.stringify(newState));
     setCollapsed(newState);
   };
 
+  const sidebarProps = useMemo(
+    (): SidebarProps => ({ toggleCollapsed }),
+    [toggleCollapsed],
+  );
+
   return (
     <aside
       className={cn(
-        'sticky overflow-y-auto w-10 transition-[width] border-r border-r-border bg-sidebar',
+        'sticky overflow-y-auto w-10 transition-[width] border-r border-r-border bg-sidebar pt-2.5',
         className,
-        collapsed ? 'w-14' : 'w-72',
+        collapsed ? collapsedSidebarWidthClass : 'w-72',
       )}
     >
-      <div
-        className={cn(
-          'flex items-center justify-between py-3 sticky top-0 bg-sidebar',
-          collapsed ? 'px-2' : 'px-4',
-        )}
+      {collapsed ? (
+        <CollapsedSidebar {...sidebarProps} />
+      ) : (
+        <ExpandedSidebar {...sidebarProps} />
+      )}
+    </aside>
+  );
+};
+
+interface SidebarProps {
+  toggleCollapsed: VoidFunction;
+}
+
+const CollapsedSidebar = ({ toggleCollapsed }: SidebarProps) => {
+  return (
+    <div
+      className={cn(
+        collapsedSidebarMaxWidthClass,
+        'group w-full flex items-center justify-center',
+      )}
+    >
+      <Image
+        className='group-hover:hidden'
+        alt={APP_NAME}
+        width={APP_LOGO_SIZE}
+        height={APP_LOGO_SIZE}
+        src={appLogo}
+      />
+
+      <Button
+        onClick={toggleCollapsed}
+        className={cn('hidden', 'group-hover:flex')}
+        size='icon'
+        variant='outline'
       >
-        {!collapsed && (
-          <div className='text-primary font-bold truncate'>{APP_NAME}</div>
-        )}
+        <ArrowRightFromLine />
+      </Button>
+    </div>
+  );
+};
+
+const ExpandedSidebar = ({ toggleCollapsed }: SidebarProps) => {
+  return (
+    <>
+      <div className='flex items-center justify-between sticky top-0 bg-sidebar pr-2'>
+        <div className='flex items-center'>
+          <div
+            className={cn(
+              collapsedSidebarWidthClass,
+              'flex items-center justify-center',
+            )}
+          >
+            <Image
+              alt={APP_NAME}
+              width={APP_LOGO_SIZE}
+              height={APP_LOGO_SIZE}
+              src={appLogo}
+            />
+          </div>
+
+          <div className='text-primary font-medium truncate'>{APP_NAME}</div>
+        </div>
+
         <Button onClick={toggleCollapsed} size='icon' variant='outline'>
-          {collapsed ? <ArrowRightFromLine /> : <ArrowLeftFromLine />}
+          <ArrowLeftFromLine />
         </Button>
       </div>
 
@@ -54,6 +120,6 @@ export const AppSidebar = ({
           <li></li>
         </ul>
       </nav>
-    </aside>
+    </>
   );
 };
