6c3a3bce32f85e5e2ac0b47cd98eb8dbbbef6731
Nikita Zhidov <nzhidov02@gmail.com>
2026-03-07 20:31:18 +0200

    Cancel previous main workflow when there is a new workflow

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -21,7 +21,9 @@ export async function proxy(request: NextRequest) {
       name: SESSION_NAME,
       value: '',
       domain,
+      path: '/',
       maxAge: 0,
+      expires: new Date(0),
     });
 
     return res;
